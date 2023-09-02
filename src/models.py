9b0dec5d5e103f9981643abff4af2b3ce58c241a
Sebastian Gaggero Díaz <49600552+SrMetus@users.noreply.github.com>
2023-09-02 13:39:30 +0000

    Finalizado

--- src/models.py
+++ src/models.py
@@ -9,35 +9,37 @@ Base = declarative_base()
 
 class Favorite(Base):
     __tablename__ = 'favorite'
-    FavoriteID = Column(Integer, ForeignKey('character.CharacterID'), primary_key=True)
-    UserID = Column(Integer, ForeignKey('user.UserID'))
+    favoriteID = Column(Integer, primary_key=True)
+    userID = Column(Integer, ForeignKey('user.UserID'))
+    characterID =Column(Integer, ForeignKey('character.CharacterID'))
 
 class Character(Base):
     __tablename__ = 'character'
-    CharacterID = Column(Integer, relationship(Favorite, Location), primary_key=True)
+    CharacterID = Column(Integer, primary_key=True)
     Name = Column(String(40), nullable=False)
-    Location = Column(String(40), nullable=False)
     Episode = Column(String(40), nullable=True)
-    Status =  Column(Integer, nullable=False)
-    StatusID = Column(Integer, relationship(Status))
+    FavoriteID = relationship('Favorite')
+    LocationID = relationship('Location')
+    StatusID = relationship('Status')
     
-
 class Status(Base):
     __tablename__ = 'status'
     StatusID = Column(Integer, ForeignKey('character.StatusID'), primary_key=True)
-
+    Status = Column(String(15), nullable=True)
 
 class User(Base):
     __tablename__ = 'user'
-    UserID = Column(Integer, relationship(Favorite), primary_key=True)
+    UserID = Column(Integer, primary_key=True)
     Name = Column(String(40), nullable=False, unique=True)
     Password = Column(String(20), nullable=False)
     Email = Column(String(60), nullable=False, unique=True)
-
+    favoriteID = relationship('Favorite')
 
 class Location(Base):
     __tablename__ = 'location'
-    LocationID = Column(Integer, ForeignKey('character.CharacterID'), primary_key=True)
+    LocationID = Column(Integer, primary_key=True)
+    Name = Column(String(50), nullable=False)
+    characterID = Column(Integer, ForeignKey('character.CharacterID'))
 
 
 
